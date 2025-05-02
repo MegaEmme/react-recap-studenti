f81d6a6a67e18a2dd11b4929eb8028ed85458282
MegaEmme <mikmusco@gmail.com>
2025-05-02 18:23:29 +0200

    feat: implement MainTemplate and MainContent components with status message

---
+++ src/components/MainTemplate.jsx
@@ -0,0 +1,12 @@
+import MainContent from "./MainContent";
+
+const MainTemplate = () => {
+    return (
+        <>
+            <h1 className="title">Gestione Studenti</h1>
+            <MainContent />
+        </>
+    )
+}
+
+export default MainTemplate;
---
+++ src/components/StatusMessage.jsx
@@ -0,0 +1,7 @@
+const StatusMessage = () => {
+    return (
+        <div id="status-message" class="status-message"></div>
+    )
+};
+
+export default StatusMessage;
